17a435c3525d7fbee1dc4a15c670cf112616e840
Paul-DDB <149811459+Paul-DDB@users.noreply.github.com>
2024-01-16 16:36:12 +0100

    Ver. 1.5
    - Ajout de la possibilité d'envoyer une consigne directement par l'opérateur via la console python. (WIP)
    - Ajout d'un fichier .bat pour télécharger tous les modules nécessaires au programme sur les PC du labo de SI.

---
+++ instaler_modules_python_labo.bat
@@ -0,0 +1,6 @@
+py -m pip --proxy ridf@10.0.0.1:3128 install opencv-python
+py -m pip --proxy ridf@10.0.0.1:3128 install datetime
+py -m pip --proxy ridf@10.0.0.1:3128 install matplotlib
+py -m pip --proxy ridf@10.0.0.1:3128 install serial
+py -m pip --proxy ridf@10.0.0.1:3128 install pyserial
+WAIT(10)
